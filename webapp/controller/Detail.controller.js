a99ac105102bf83387bf0fa87cebc830b46896e7
gmahi <gullamahi@gmail.com>
2016-12-22 23:48:16 +0000

    Using formatters

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,9 +1,13 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/core/routing/History"
-], function(Controller, History) {
+	"sap/ui/core/routing/History",
+	"sapui5/demo/mvcapp/model/formatter",
+	"sapui5/demo/mvcapp/model/types"
+], function(Controller, History, formatter, types) {
 	"use strict";
 	return Controller.extend("sapui5.demo.mvcapp.controller.Detail", {
+		formatter : formatter,
+		types : types,
 		onInit: function() {
 			this._oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			this._oRouter.getRoute("detail").attachPatternMatched(this._onDetailMatched, this);
---
+++ webapp/model/types.js
@@ -0,0 +1,25 @@
+sap.ui.define([
+			"sap/ui/model/SimpleType"
+		], function(SimpleType) {
+			"use strict";
+			return {
+				/**
+				 * DataTypeforphonenumbers.
+				 *
+				 * @public
+				 */
+				PhoneNumber: SimpleType.extend("sap.test.phoneNumber", {
+						formatValue: function(oValue) {
+							return "Phonenumber:" + oValue;
+						},
+						parseValue: function(oValue) {
+							return oValue;
+						},
+						validateValue: function(oValue) {
+							if (!/\+*\D*[0-9]*\-([2-9]\d{2})(\D*)([2-9]\d{2})(\D*)(\d{4})\D/.test(oValue)) {
+							throw new sap.ui.model.ValidateException("phone number must follow the pattern +1 234-567-890!");
+								}
+							}
+						})
+				};
+			});
