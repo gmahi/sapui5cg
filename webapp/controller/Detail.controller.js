e62be35f8940078fa34cf943e5857917b22f94b1
gmahi <gullamahi@gmail.com>
2016-12-27 10:17:04 +0000

    Using Factory

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -2,12 +2,13 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/core/routing/History",
 	"sapui5/demo/mvcapp/model/formatter",
-	"sapui5/demo/mvcapp/model/types"
-], function(Controller, History, formatter, types) {
+	"sapui5/demo/mvcapp/model/types",
+	"sap/ui/layout/VerticalLayout"
+], function(Controller, History, formatter, types, VerticalLayout) {
 	"use strict";
 	return Controller.extend("sapui5.demo.mvcapp.controller.Detail", {
-		formatter : formatter,
-		types : types,
+		formatter: formatter,
+		types: types,
 		onInit: function() {
 			this._oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			this._oRouter.getRoute("detail").attachPatternMatched(this._onDetailMatched, this);
@@ -19,6 +20,52 @@ sap.ui.define([
 				"/Suppliers/" + oEvent.getParameter("arguments").ID;
 			var oView = this.getView();
 			oView.bindElement(sObjectPath);
+			this._createProductsAggregation();
+		},
+
+		/**
+		 * Creates the line items for the products table using a factory.
+		 *
+		 * @function
+		 * @private
+		 */
+		_createProductsAggregation: function() {
+			var oTable = this.getView().byId("table");
+
+			oTable.bindAggregation("items", "Products", function(sId, oContext) {
+				var sAllergenics = oContext.getProperty("Allergenics");
+				var oColumnListItem = new sap.m.ColumnListItem(sId);
+				oColumnListItem.addCell(new sap.m.ObjectIdentifier({
+					text: "{ID}"
+				}));
+
+				if (sAllergenics) {
+					// we have found allergenics, so we provide a VerticalLayout instead
+					// of just displaying the product name. The VerticalLayout then takes
+					// the product name plus the allergenics into its own content aggregation
+					oColumnListItem.addCell(new VerticalLayout({
+						content: [
+							new sap.m.Text({
+								text: "{Name}"
+							}),
+							new sap.m.Text({
+								text: "{Allergenics}"
+							})
+						]
+					}));
+				} else {
+					// no allergenics there, we display the name as usual
+					oColumnListItem.addCell(new sap.m.ObjectIdentifier({
+						text: "{Name}"
+					}));
+				}
+
+				oColumnListItem.addCell(new sap.m.ObjectNumber({
+					number: "{Price}",
+					unit: "USD"
+				}));
+				return oColumnListItem;
+			});
 		},
 
 		onNavPress: function() {
@@ -42,8 +89,8 @@ sap.ui.define([
 		 * @param{string}sNamestringtobeformatted
 		 * @public
 		 */
-	
-	 	toUpperCase: function(sName) {
+
+		toUpperCase: function(sName) {
 			return sName && sName.toUpperCase();
 		}
 
